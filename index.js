4ac125eb7f9a5735be5954963c82f5a43d69c5a2
Lumi <ludmila.saba29@gmail.com>
2022-09-27 22:22:11 -0300

    Cambios con JS

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+const dropdownElementList = document.querySelectorAll('dropdown-toggle')
+const dropdownList = [dropdownElementList].map(dropdownToggleEl => new bootstrap.Dropdown(dropdownToggleEl))
 
 const carousel = new bootstrap.Carousel('1PRENDI')
+
+
+
+document.getElementById("buttonCarousel1").onclick = function () {
+    location.href = "./info.html";
+};
+
+document.getElementById("buttonCarousel2").onclick = function () {
+    location.href = "./info.html";
+};
+
+document.getElementById("buttonCarousel3").onclick = function () {
+    location.href = "./info.html";
+};
